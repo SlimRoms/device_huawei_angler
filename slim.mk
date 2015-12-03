08f7d3053276db8a7316ff1431a84326b10a89b6
Daniel Koman <dankoman30@gmail.com>
2015-12-03 20:15:59 +0100

    Slimify / Start with MDB08M fingerprint
    Change-Id: I87d28488191274eafe5bcae2c497ff04977e8be7

---
+++ slim.mk
@@ -0,0 +1,28 @@
+# Release name
+PRODUCT_RELEASE_NAME := angler
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/angler/aosp_angler.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := angler
+PRODUCT_NAME := slim_angler
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 6P
+PRODUCT_MANUFACTURER := huawei
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0/MDB08M/2353240:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0 MDB08M 2353240 release-keys"
