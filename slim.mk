1f193e49ceb174d45e2ce52875ec64f4f57eedc7
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2016-09-08 20:20:59 +0200

    angler: update fingerprint to MTC20L
    Change-Id: Icc14a13c56bc866193610442a1c7cd4fead52264
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- slim.mk
+++ slim.mk
@@ -42,5 +42,5 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20F/3031278:user/release-keys \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20F 3031278 release-keys"
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MTC20L/3230295:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MTC20L 3230295 release-keys"
