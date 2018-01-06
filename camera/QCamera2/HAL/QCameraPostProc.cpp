49b955d50938c96ea296fdad74c48262c67d0d65
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2018-01-06 20:21:03 +0100

    Camera HAL3: cast long int to int for liblog to fix compiling
    Change-Id: I63ed8ff2b5c406723f05b0ffbb00afe6d5d99d63
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- camera/QCamera2/HAL/QCameraPostProc.cpp
+++ camera/QCamera2/HAL/QCameraPostProc.cpp
@@ -2365,11 +2365,11 @@ void *QCameraPostProcessor::dataSaveRoutine(void *data)
                         if ((ssize_t)job_data->out_data.buf_filled_len != written_len) {
                             ALOGE("%s: Failed save complete data %d bytes "
                                   "written instead of %d bytes!",
-                                  __func__, written_len,
+                                  __func__, (int) written_len,
                                   job_data->out_data.buf_filled_len);
                         } else {
                             CDBG_HIGH("%s: written number of bytes %d\n",
-                                __func__, written_len);
+                                __func__, (int) written_len);
                         }
 
                         close(file_fd);
